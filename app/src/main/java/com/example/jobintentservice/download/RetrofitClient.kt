4c3331bbd2bfc297675ecbdd7290fd032b849418
LeeJangHee <56954702+LeeJangHee@users.noreply.github.com>
2021-12-15 01:11:59 +0900

    - URL 수정

--- app/src/main/java/com/example/jobintentservice/download/RetrofitClient.kt
+++ app/src/main/java/com/example/jobintentservice/download/RetrofitClient.kt
@@ -8,21 +8,9 @@ object RetrofitClient {
     private var instance: Retrofit? = null
     private val gson = GsonBuilder().setLenient().create()
 
-    // http://wkdgml96.iptime.org:8080/image/app-debug.apk
-    // https://vc-apl-web.s3.ap-northeast-2.amazonaws.com/apk/download-test-apk.apk
-//    const val BASE_URL = "https://vc-apl-web.s3.ap-northeast-2.amazonaws.com"
-//    const val FILE_URL = "/apk/download-test-apk.apk"
+
     const val BASE_URL = "http://wkdgml96.iptime.org:8080/"
     const val FILE_URL = "image/app-debug.apk"
-
-    const val BASE_URL1 = "https://vc-apl-web.s3.ap-northeast-2.amazonaws.com/apk/"
-    const val FILE_URL1 = "download-test-apk.apk"
-
-    const val BASE_URL2 = "https://vse-s3demo.voicecaddie.co.kr/webview/app-deploy/AND/"
-    const val FILE_URL2 = "com.vcinc.vse-debug-0.9.1.21030803.apk"
-
-    const val BASE_URL3 = "https://vse-s3demo.voicecaddie.co.kr/webview/app-deploy/"
-    const val FILE_URL3 = "index.html"
 
     const val BASE_URL4 = "https://youtu.be/"
     const val FILE_URL4 = "B4gFbWnNpac"
@@ -31,7 +19,7 @@ object RetrofitClient {
     fun getInstance(): Retrofit {
         if (instance == null) {
             instance = Retrofit.Builder()
-                .baseUrl(BASE_URL1)
+                .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
